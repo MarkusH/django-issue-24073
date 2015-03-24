0399a47eb9d15df09cf761542aa70cfeb563b91b
Markus Holtermann <info@markusholtermann.eu>
2015-03-24 15:27:17 +0100

    Add overlapped squashed migrations

---
+++ app_b/migrations/0005_auto_20141119_1937.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('app_b', '0002_squashed_0004_auto_20141001_1035'),
+    ]
+
+    operations = [
+    ]
